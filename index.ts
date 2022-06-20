3fb6bba16d577f1ea24b9f72af1eb3b944d278e8
Christian Nuss <cnuss@users.noreply.github.com>
2022-06-20 17:58:34 +0000

    update paths

--- index.ts
+++ index.ts
@@ -5,13 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   return res.status(200).json({
-    message: 'Hello from root!',
-  });
-});
-
-app.get('/hello', (req, res) => {
-  return res.status(200).json({
-    message: 'Hello from path!',
+    message: 'Hello world!',
   });
 });
 
